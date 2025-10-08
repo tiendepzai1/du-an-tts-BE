6cd84f667f2351925276d3355ac9bbb9d214e292
le van tien <ngaule29@gmail.com>
2025-10-08 16:09:50 +0700

    commit

--- src/index.js
+++ src/index.js
@@ -9,7 +9,24 @@ import cors from "cors";
 
 const app = express();
 
-app.use(cors());
+const allowedOrigins = [
+  "https://du-an-tts.vercel.app", // domain frontend thật
+  "http://localhost:5173",        // nếu bạn test local
+];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true, // 🔥 cho phép cookie/token
+  })
+);
+
 // kết nối cơ sở dữ liệu
 mongoose.connect(`mongodb+srv://ngaule29_db_user:levantien123@cluster0.b5g0a6x.mongodb.net/db_base333?retryWrites=true&w=majority&appName=Cluster0`)
   .then(() => console.log('Connected to MongoDB'))
